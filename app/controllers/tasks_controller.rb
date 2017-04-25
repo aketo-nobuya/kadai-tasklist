fb3abd60ce1fab1ba61e1faeb5217671470f0fbc
tanaka.taro <tanaka.taro@gmail.com>
2017-04-25 05:07:21 +0000

    kari

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all.page(params[:page])
@@ -52,5 +53,12 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_path
+    end
   end
 end
